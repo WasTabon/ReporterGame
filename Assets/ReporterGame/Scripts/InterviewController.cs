71e07c96757654e4e2f70366fa17ec46ef7ae006
WasTabon <titanium.jen@gmail.com>
2025-10-29 20:19:15 +0200

    ui

--- Assets/ReporterGame/Scripts/InterviewController.cs
+++ Assets/ReporterGame/Scripts/InterviewController.cs
@@ -7,6 +7,21 @@ public class InterviewController : MonoBehaviour
 {
     public static InterviewController Instance;
 
+    [SerializeField] private GameObject background;
+    [SerializeField] private GameObject interviewPanel;
+    [SerializeField] private GameObject personBackground;
+    [SerializeField] private GameObject dialogueBackground;
+    [SerializeField] private GameObject dialogueOpponent1;
+    [SerializeField] private GameObject dialoguePlayer1;
+    [SerializeField] private GameObject optionsBackground;
+    [SerializeField] private GameObject dialoguePlayer2;
+    [SerializeField] private GameObject dialogueOpponent2;
+    [SerializeField] private GameObject continueButton;
+
+    [SerializeField] private Button[] optionButtons;
+
+    [SerializeField] private float animationDuration = 0.5f;
+
     private void Awake()
     {
         Instance = this;
@@ -14,8 +29,45 @@ public class InterviewController : MonoBehaviour
 
     private void Start()
     {
+        PrepareUI();
         HideAllButtons();
         StartCoroutine(InterviewRoutine());
+    }
+
+    private void PrepareUI()
+    {
+        background.SetActive(false);
+        interviewPanel.SetActive(false);
+        personBackground.SetActive(false);
+        dialogueBackground.SetActive(false);
+        dialogueOpponent1.SetActive(false);
+        dialoguePlayer1.SetActive(false);
+        optionsBackground.SetActive(false);
+        dialoguePlayer2.SetActive(false);
+        dialogueOpponent2.SetActive(false);
+        continueButton.SetActive(false);
+
+        SetAlpha(background, 0f);
+        SetAlpha(interviewPanel, 0f);
+        personBackground.transform.localScale = Vector3.zero;
+        dialogueBackground.transform.localScale = Vector3.zero;
+        dialogueOpponent1.transform.localScale = Vector3.zero;
+        dialoguePlayer1.transform.localScale = Vector3.zero;
+        optionsBackground.transform.localScale = Vector3.zero;
+        dialoguePlayer2.transform.localScale = Vector3.zero;
+        dialogueOpponent2.transform.localScale = Vector3.zero;
+        continueButton.transform.localScale = Vector3.zero;
+
+        foreach (Button button in optionButtons)
+        {
+            button.onClick.AddListener(() => OnOptionSelected());
+        }
+
+        Button contButton = continueButton.GetComponent<Button>();
+        if (contButton != null)
+        {
+            contButton.onClick.AddListener(OnContinueClicked);
+        }
     }
 
     private void HideAllButtons()
@@ -75,5 +127,85 @@ public class InterviewController : MonoBehaviour
     {
         button.transform.DOScale(0f, 0.3f).SetEase(Ease.InBack);
         Debug.Log("Interview button clicked!");
+        
+        StartCoroutine(ShowInterviewSequence());
+    }
+
+    private IEnumerator ShowInterviewSequence()
+    {
+        background.SetActive(true);
+        DOTween.To(() => GetAlpha(background), x => SetAlpha(background, x), 1f, animationDuration);
+        yield return new WaitForSeconds(animationDuration);
+
+        interviewPanel.SetActive(true);
+        DOTween.To(() => GetAlpha(interviewPanel), x => SetAlpha(interviewPanel, x), 1f, animationDuration);
+        yield return new WaitForSeconds(animationDuration);
+
+        personBackground.SetActive(true);
+        personBackground.transform.DOScale(1f, animationDuration).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(animationDuration);
+
+        dialogueBackground.SetActive(true);
+        dialogueBackground.transform.DOScale(1f, animationDuration).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(animationDuration);
+
+        dialogueOpponent1.SetActive(true);
+        dialogueOpponent1.transform.DOScale(1f, animationDuration).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(animationDuration);
+
+        dialoguePlayer1.SetActive(true);
+        dialoguePlayer1.transform.DOScale(1f, animationDuration).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(animationDuration);
+
+        optionsBackground.SetActive(true);
+        optionsBackground.transform.DOScale(1f, animationDuration).SetEase(Ease.OutBack);
+    }
+
+    private void OnOptionSelected()
+    {
+        StartCoroutine(ShowResponseSequence());
+    }
+
+    private IEnumerator ShowResponseSequence()
+    {
+        optionsBackground.transform.DOScale(0f, animationDuration).SetEase(Ease.InBack);
+        yield return new WaitForSeconds(animationDuration);
+        optionsBackground.SetActive(false);
+
+        dialoguePlayer2.SetActive(true);
+        dialoguePlayer2.transform.DOScale(1f, animationDuration).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(animationDuration);
+
+        dialogueOpponent2.SetActive(true);
+        dialogueOpponent2.transform.DOScale(1f, animationDuration).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(animationDuration);
+
+        continueButton.SetActive(true);
+        continueButton.transform.DOScale(1f, animationDuration).SetEase(Ease.OutBack);
+    }
+
+    private void OnContinueClicked()
+    {
+        Debug.Log("Continue button clicked!");
+    }
+
+    private void SetAlpha(GameObject obj, float alpha)
+    {
+        CanvasGroup canvasGroup = obj.GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+        {
+            canvasGroup = obj.AddComponent<CanvasGroup>();
+        }
+        canvasGroup.alpha = alpha;
+    }
+
+    private float GetAlpha(GameObject obj)
+    {
+        CanvasGroup canvasGroup = obj.GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+        {
+            canvasGroup = obj.AddComponent<CanvasGroup>();
+        }
+        return canvasGroup.alpha;
     }
 }
