1f9aef18e3cb25c96722189b7d12bbfdfd8ba25e
WasTabon <72084063+WasTabon@users.noreply.github.com>
2025-11-19 00:38:44 +0200

    ui

--- Assets/ReporterGame/Scripts/UIController.cs
+++ Assets/ReporterGame/Scripts/UIController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 public class UIController : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _moneyText;
+    [SerializeField] private TextMeshProUGUI _moneyText2;
+    [SerializeField] private TextMeshProUGUI _reputationText;
 
     private void Update()
     {
         _moneyText.text = $"{WalletController.Instance.Money}";
+        _moneyText2.text = $"{WalletController.Instance.Money}";
+        _reputationText.text = $"{WalletController.Instance.Reputation}";
     }
 }
